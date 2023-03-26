a380ccdc1157f59b5e2123ff73ceab0d038d4b31
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-26 23:29:36 +0100

    Update QRCodeScanner to display message from server

--- frontend/src/components/dashboard/pages/qrCodeScanner/QRCodeScanner.jsx
+++ frontend/src/components/dashboard/pages/qrCodeScanner/QRCodeScanner.jsx
@@ -1,7 +1,7 @@
 import './qrCodeScanner.css';
 import './qrCodeScannerMobile.css';
 
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 import {QrScanner} from '@yudiel/react-qr-scanner';
 import httpClient from "../../../../httpClient";
@@ -14,56 +14,73 @@ const QRCodeScanner = (props) => {
     const setUser = props.setUser;
     let event = props.event;
     let [validImg, setValidImg] = useState();
+    let [ticketMsg, setTicketMsg] = useState();
+    const [hasScanned, setHasScanned] = useState(false);
+    const scannerRef = useRef(null);
 
     const navigate = useNavigate();
 
     // Once user is updated, check if valid
     useEffect(() => {
-        if (user == null || (user != null && user.role !== "management")) {
+        if (user == null || (user.role !== "management")) {
             navigate("/")
         }
     }, [user])
 
+    const handleDecode = (result) => {
+        if (hasScanned) {
+            return;
+        }
+
+        setHasScanned(true);
+
+        const data = {
+            event_id: event.event_id,
+            qr_data: result
+        }
+
+        httpClient.post(`/ticket/validate`, data, {
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": getCookie("csrf_access_token"),
+            }
+        })
+        .then(response => {
+            // valid ticket
+            setValidImg(Tick);
+            setTicketMsg(response.msg);
+        })
+        .catch(error => {
+            console.log(error)
+            if (error.response) {
+                // invalid ticket
+                setValidImg(Cross);
+                setTicketMsg(error.response.data.msg)
+            }
+        });
+
+        scannerRef.current.stop();
+    }
+
+    let content;
+    if (!hasScanned) {
+        content = <QrScanner scanDelay={15000}
+                             onDecode={handleDecode}
+                             onError={(error) => console.log(error?.message)} />;
+    } else {
+        content =
+            <div>
+                <img className={'validImg'} src={validImg} />
+                <p className={'ticketResponse'}>
+                    {validImg === Cross ? <span style={{ color: "red"}}>Error: </span> : ''}
+                    {ticketMsg}
+                </p>
+            </div>;
+    }
+
     return (
         <div className={'contentContainer qrScannerDiv'}>
-            <QrScanner
-                scanDelay={7500}
-                onDecode={(result) => {
-                    console.log(result)
-                    const data = {
-                        event_id: event.event_id,
-                        qr_data: result
-                    }
-
-                    httpClient.post(`/ticket/validate`, data, {
-                        headers: {
-                            "Content-Type": "application/json",
-                            "X-CSRF-TOKEN": getCookie("csrf_access_token"),
-                        }
-                    })
-                    .then(response => {
-                        // valid ticket
-                        setValidImg(Tick);
-                        setTimeout(() => {
-                            setValidImg(undefined);
-                        }, 3000);
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        if (error.response && error.response.status === 400) {
-                            // invalid ticket
-                            setValidImg(Cross);
-                            setTimeout(() => {
-                                setValidImg(undefined);
-                            }, 3000);
-                        }
-                    });
-                }}
-                onError={(error) => console.log(error?.message)}
-            />
-
-            <img className={'validImg'} src={validImg} />
-            <div></div>
+            {content}
         </div>
     )
 }
